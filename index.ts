44c0430e45c15c97ed02cc93a3ea78fbba6be39c
Arcedo <arcedo.marc@gmail.com>
2024-10-30 21:45:06 +0100

    fix issues

--- index.ts
+++ index.ts
@@ -2,4 +2,4 @@ import { Server } from './src/models/Server.model';
 
 export const server: Server = new Server('/api');
 
-server.listen(Number(process.env.SERVER_PORT) | 3000);
+server.listen(Number(process.env.SERVER_PORT) || 3000);
